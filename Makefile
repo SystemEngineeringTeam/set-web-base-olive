bc62bd79c193c53980b25607b970180ba279bd6d
YoshikawaTaiki <yoshikawataiki@gmail.com>
2018-12-05 23:10:03 +0900

    update

---
+++ Makefile
@@ -0,0 +1,26 @@
+docker/build:
+	docker-compose build
+
+docker/start:
+	docker-compose up -d
+
+docker/logs:
+	docker-compose logs
+
+docker/stop:
+	docker-compose stop
+
+docker/clean:
+	docker-compose rm
+	
+react/bash:
+	docker-compose exec react bash
+
+npm/install:
+	docker-compose exec react npm install
+
+npm/start:
+	docker-compose exec react npm run watch
+
+npm/build:
+	docker-compose exec react npm run build
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/main.tsx'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: './js/main.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: './css/main.css'
+    })
+  ],
+  resolve: {
+    extensions: [
+      '.ts',  // for ts-loader
+      '.tsx',  // for ts-loader
+      '.js'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+	    MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: './images/[name].[ext]',
+            outputPath: './',
+            publicPath: path => '.' + path
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  }
+}
